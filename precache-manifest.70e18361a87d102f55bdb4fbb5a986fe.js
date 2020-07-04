26ebf15ad6007d93afd986108ce824774b13c75b
Self <bumi0823@gmail.com>
2020-07-04 20:47:09 +0900

    Updates

---
+++ precache-manifest.70e18361a87d102f55bdb4fbb5a986fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c30049dabf8e5eed54170654164f14",
+    "url": "/base_app_react/index.html"
+  },
+  {
+    "revision": "f1ad38c5f7bfe5539bed",
+    "url": "/base_app_react/static/css/main.8d94b5ef.chunk.css"
+  },
+  {
+    "revision": "44e26568f6729415a870",
+    "url": "/base_app_react/static/js/2.52c8a92d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/base_app_react/static/js/2.52c8a92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ad38c5f7bfe5539bed",
+    "url": "/base_app_react/static/js/main.7d73a52e.chunk.js"
+  },
+  {
+    "revision": "5be7775daed53971f78e",
+    "url": "/base_app_react/static/js/runtime-main.c51aa1ed.js"
+  }
+]);
