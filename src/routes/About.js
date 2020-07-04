bed6c2adfb921b69694dc65a6bb6cfc63dfa5087
Self <bumi0823@gmail.com>
2020-07-04 20:44:12 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return (
+        <div className="about__container">
+            <span>
+                "Doby is Free"
+            </span>
+            <span>- Me</span>
+        </div>
+    )
+}
+
+export default About;
